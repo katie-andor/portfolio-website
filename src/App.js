6222a5ed33e87cd4acb028fff268cd8125ad1774
katie-andor <superkga5678@gmail.com>
2024-10-03 17:50:38 -0400

    added routing and stuff to the home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home'; // Import your home component
+import About from './About'; // Example of another component
+import NotFound from './NotFound'; // Example for a 404 page
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} /> {/* Home page */}
+      <Route path="/about" element={<About />} /> {/* Example route */}
+      <Route path="*" element={<NotFound />} /> {/* 404 page */}
+    </Routes>
   );
-}
+};
 
 export default App;
