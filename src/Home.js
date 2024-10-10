43eaffb7064d47cc26a1cc7619b35c8a9927266d
katie-andor <superkga5678@gmail.com>
2024-10-09 22:14:02 -0400

    added styling to post-it

--- src/Home.js
+++ src/Home.js
@@ -14,31 +14,29 @@ const Home = (props) => {
         <img className="ml-2 mr-2 w-[clamp(30px,3vw,300px)]" src={internetIcon} alt="Internet Icon" />
       </Link>
       
-      {/* Responsive Grid Container */}
       <div className="
-        grid gap-1 m-8 mr-[3%]
-        grid-cols-2       /* 1 column on extra small screens (default) */
-        sm:grid-cols-3    /* 2 columns on small screens */
-        md:grid-cols-3    /* 3 columns on medium screens */
-        lg:grid-cols-[31%_31%_37%]  /* Custom percentages for large screens */
-        xl:grid-cols-[31%_31%_37%]  /* Custom percentages for extra large screens */
+        grid gap-4 m-8 mr-[3%]
+        grid-cols-2
+        sm:grid-cols-3
+        md:grid-cols-3
+        lg:grid-cols-[33%_31%_37%]
+        xl:grid-cols-[33%_31%_37%]
       ">
         
-        {/* Post-it Section */}
-        <div className="bg-[#FFFA89] p-8 sm:col-span-1 lg:row-span-2">
-          <h1>PROJECTS:</h1>
-          <h2>Esports Research Website:</h2>
-          <ul>
+        <div className="bg-[#FFFA89] p-8 sm:col-span-1 lg:row-span-2 pt-[1%] pb-[3%] pr-[2%] pl-[2%]" style={{ boxShadow: '11px 11px 10px rgba(0, 0, 0, 0.25)' }}>
+          <h1 className=" text-[30px] sm:text-[30px] md:text-[45px] lg:text-[60px] xl:text-[65px] leading-none mt-0 mb-0">PROJECTS:</h1>
+          <h2 className="text-[23px] sm:text-[23px] md:text-[30px] lg:text-[30px] xl:text-[38px] leading-none">Esports Research Website:</h2>
+          <ul className=" list-disc text-[20px] sm:text-[20px] md:text-[23px] lg:text-[23px] xl:text-[27px] leading-none ml-[8%]">
             <li>Uses WordPress, Divi Theme Builder</li>
             <li>esportsresearch.net</li>
           </ul>
-          <h2>Cuphead Fan Wiki:</h2>
-          <ul>
+          <h2 className="text-[23px] sm:text-[23px] md:text-[30px] lg:text-[30px] xl:text-[38px] leading-none">Cuphead Fan Wiki:</h2>
+          <ul className=" list-disc text-[20px] sm:text-[20px] md:text-[23px] lg:text-[23px] xl:text-[27px] leading-none ml-[8%]">
             <li>Uses React.js, HTML, and CSS</li>
             <li>cupheadfanwiki.netlify.app</li>
           </ul>
-          <h2>GitHub:</h2>
-          <ul>
+          <h2 className="text-[23px] sm:text-[23px] md:text-[30px] lg:text-[30px] xl:text-[38px] leading-none">GitHub:</h2>
+          <ul className=" list-disc text-[20px] sm:text-[20px] md:text-[23px] lg:text-[23px] xl:text-[27px] leading-none ml-[8%]">
             <li>https://github.com/katie-andor</li>
           </ul>
         </div>
