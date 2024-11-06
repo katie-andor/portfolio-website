17a789b5001f64d45fd80683c8ab063b7b547546
katie-andor <superkga5678@gmail.com>
2024-11-06 13:57:19 -0500

    changed favicon and put resume pdf on resume page, not mobile responsive as of now

--- src/Resume.js
+++ src/Resume.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React from "react";
 import internetIcon from "./images/icons-8-internet-icon.png";
 import { Link } from "react-router-dom";
+import resume from "./images/KAITLYNANDOR_Resume.pdf";
+import Footer from "./components/Footer";
 
 const Resume = () => {
   return (
-    <div>
-    <Link to="/">
+    <div className="min-h-screen flex flex-col">
+      <Link to="/">
         <img
           className="ml-2 mr-2 w-[40px] pt-[10px]"
           src={internetIcon}
           alt="Internet Icon"
         />
       </Link>
-    <p>resume</p>
+      <div className="flex-grow flex justify-center">
+        <iframe src={resume} className="w-[90%] h-[80vh]" />
+      </div>
+      <Footer />
     </div>
   );
 };
